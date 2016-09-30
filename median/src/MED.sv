c021246aeecd1ee549e993c237444b898b870295
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-09-30 17:41:08 +0200

    Ajout module MED

---
+++ median/src/MED.sv
@@ -0,0 +1,12 @@
+module MED #(parameter SIZE = 8, NB_PIXEL = 9)(
+        input [SIZE-1:0]DI,
+        input logic DSI,
+        input logic BYP,
+        input logic CLK,
+        output [SIZE-1:0]DO
+        );
+
+
+
+endmodule
+
