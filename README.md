0979d11064af3ca8545051e379510a7d477295f9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-09-30 18:50:12 +0200

    1ere ébauche de MED, non compilable

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Bienvenue sur le gitLab de Erwan CHERIAUX !
 
 07/10/2016: Filtre médian
 
-	Module MCE:		Compare 2 nombres et les renvois triés 
+	Module MCE:		Compare 2 nombres et les renvoie triés 
 	Module MCE-testbench: 	Test sur mille valeurs randoms le module MCE
 
 La simulation sur ModelSim renvoie un message de confirmation du bon fonctionnement du module MCE.
@@ -14,3 +14,8 @@ IMPORTANT: Il faut compiler le fichier testbench.sv avant de réaliser la simula
 
 Ajout d'un Makefile capable de synthétiser les modules et de renseigner l'optimalité du code.
 La synthèse du module MCE indique bien 24 LUTs, ce qui est conforme à l'optimalité attendu.
+
+	Module MED: 		Récupere n pixel et renvoie la médiane
+
+Première ébauche, le code ne compile pas !!!
+ERREUR: ** Error: ../src/MED.sv(20): (vlog-2110) Illegal reference to net "registre".
--- median/src/MED.sv
+++ median/src/MED.sv
@@ -6,7 +6,41 @@ module MED #(parameter SIZE = 8, NB_PIXEL = 9)(
         output [SIZE-1:0]DO
         );
 
+integer i;
+wire [SIZE-1:0] registre [NB_PIXEL-1:0];
+wire [SIZE-1:0] MAX, MIN;
 
+//Module MCE
+MCE #(.SIZE(SIZE)) dut(.A(registre[NB_PIXEL-1]), .B(registre[NB_PIXEL-2]), .MAX(MAX), .MIN(MIN));
+
+//registre à décalage de R0 à R7
+always@(posedge CLK)
+for (i=0;i<=NB_PIXEL-3;i=i+1) 
+begin
+	registre[i+1] <= registre[i];
+end
+
+//Gestion de l'entrée BYP
+always@(posedge CLK)
+if (BYP) registre[NB_PIXEL-1] <= registre[NB_PIXEL-2];
+else 	 registre[NB_PIXEL-1] <= MAX;
+
+//Gestion de l'entrée DSI
+assign registre[0] = (DSI)? DI: MIN;
+
+//Gestion de la sortie DO
+assign DO = registre[NB_PIXEL-1];
 
 endmodule
 
+/*
+for iter in 0 ... PixNum/2
+begin
+  for pix_idx in 1 ... PixNum-iter-1
+  begin
+    if (V[0] < V[pix_idx])  Exchange(V[0],V[pix_idx])
+  end
+  med = V[0]
+  V[0:PixNum-1] = {V[1:PixNum-1],0}
+end
+*/
