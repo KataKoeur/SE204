99f260e5555160979f0f4c5e3ce4f6f320838af9
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-10-01 18:20:26 +0200

    Ajout module MEDIAN

---
+++ median/src/MEDIAN.sv
@@ -0,0 +1,11 @@
+module MED #(parameter SIZE = 8)(
+        input logic [SIZE-1:0]DI,
+        input logic DSI,
+        input logic nRST,
+        input logic CLK,
+        output logic [SIZE-1:0]DO,
+        output logic DSO
+        );
+
+
+endmodule
