6bb5fa9ea56772d74b9e2df06237f2d1e2d7b7c2
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-10-05 15:53:47 +0200

    Synthese de MEDIAN

--- README.md
+++ README.md
@@ -13,16 +13,18 @@ La simulation sur ModelSim renvoie un message de confirmation du bon fonctionnem
 IMPORTANT: Il faut compiler le fichier testbench.sv avant de réaliser la simulation.
 
 Ajout d'un Makefile capable de synthétiser les modules et de renseigner l'optimalité du code.
-La synthèse du module MCE indique bien 24 LUTs, ce qui est conforme à l'optimalité attendu.
+La synthèse du module MCE indique bien 24 LUTs, ce qui est conforme à l'optimalité attendue.
 
 	Module MED: 		Récupere n pixel et renvoie la médiane
         Module MED-testbench:   Test sur mille valeurs randoms le module MED
 
 Le module MED est terminé et a passé la phase de simulation avec succes.
 La synthétisation du module MED nous informe que le code utilise 25 LUTs ce qui me semble 
-peu mais optimale. Le schéma RTL correspond à ce que l'on attend. Le slack vaut 16.254ns sur 20ns.
+peu mais optimal. Le schéma RTL correspond à ce que l'on attend. Le slack vaut 16.254ns sur 20ns.
 
 	Module MEDIAN:			Exploite le module MED via un algo adapté à 9 pixels
         Module MEDIAN-testbench:  	Test le module MEDIAN
 
 Le module MEDIAN compile. La simulation est une réussite fulgurante.
+Après quelques modifications dans le fichier MEDIAN.sv suite aux erreurs de la synthèse. 
+J'obtiens les résultats suivants: LUTs = 51, registres = 14, slack = 16.227
--- median/src/MEDIAN.sv
+++ median/src/MEDIAN.sv
@@ -15,7 +15,7 @@ MED I_MED(.DI(DI), .DSI(DSI), .BYP(BYP), .CLK(CLK), .DO(DO));
 
 //Gestion de la sortie DSO
 always@(*)
-DSO = (cpt>40);
+DSO <= (cpt>40);
 
 //Le compteur s'incrémente à chaque coup d'horloge
 //et permet de faire avancer l'automate
@@ -24,36 +24,26 @@ always@(posedge CLK)
 if(!nRST)
         begin
         cpt <= 0;
-        DSO <= 0;
-        BYP <= 0;
         end
 else
-        begin
-        if(DSI) //Lecture des pixels en entrée
-                begin
-                cpt <= 0;
-                DSO <= 0;
-                end
-        else    //Tous les pixels sont présent, Lancement de l'algo
-                begin
-                cpt <= cpt + 1;
-                end
-        end
+        if(DSI) cpt <= 0;       //Lecture des pixels en entrée
+        else    cpt <= cpt + 1; //Tous les pixels sont présent, Lancement de l'algo
 
 //Machine à état
 always@(*)
-if(DSI)	BYP <= 1;
+if(DSI)	       BYP <= 1;
+else if(!nRST) BYP <= 0;
 else
 	case (cpt)
-		6'd0   : BYP = 0;
-		6'd9   : BYP = 1;
-		6'd10  : BYP = 0;
-		6'd17  : BYP = 1;
-		6'd19  : BYP = 0;
-		6'd25  : BYP = 1;
-		6'd28  : BYP = 0;
-		6'd33  : BYP = 1;
-		6'd37  : BYP = 0;
+		6'd0   : BYP <= 0;
+		6'd9   : BYP <= 1;
+		6'd10  : BYP <= 0;
+		6'd17  : BYP <= 1;
+		6'd19  : BYP <= 0;
+		6'd25  : BYP <= 1;
+		6'd28  : BYP <= 0;
+		6'd33  : BYP <= 1;
+		6'd37  : BYP <= 0;
 	endcase
 
 endmodule
