2520599a9b1343425f5fb249e05344e27709be83
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-10-21 14:52:47 +0200

    Synthèse de l'étape 1 ternminée

--- projet_video/fpga/synth/file_list.tcl
+++ projet_video/fpga/synth/file_list.tcl
@@ -1,3 +1,4 @@
 # La liste des fichiers source à utiliser
 
 set_global_assignment -name SYSTEMVERILOG_FILE ${TOPDIR}/fpga/src/fpga.sv
+set_global_assignment -name SYSTEMVERILOG_FILE ${TOPDIR}/fpga/src/reset.sv
