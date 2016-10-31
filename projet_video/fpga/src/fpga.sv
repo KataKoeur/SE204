057924b5d0519f2b15e8dd680915f24ed86ec716
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-11-01 00:30:23 +0100

    Implémentation du module sdram dans le module fpga

--- projet_video/fpga/src/fpga.sv
+++ projet_video/fpga/src/fpga.sv
@@ -15,7 +15,8 @@ module fpga(// port d'entrée
 	output logic fpga_LEDR3,
 	output logic fpga_SEL_CLK_AUX,
 	// interface
-	vga_if.master vga_ifm
+	vga_if.master vga_ifm,
+	sdram_if.master sdram_ifm
 	);
 
 //parametres
@@ -38,9 +39,59 @@ logic [CPT_50_W-1:0]CPT_50;
 logic [CPT_27_W-1:0]CPT_27;
 logic n_rst;
 
+// Instanciation de la PLL pour générer la clock wishbone et la clock sdram
+logic wshb_clk ;
+logic wshb_rst ;
+logic sdram_clk;
+logic locked ;
+
 //modules
-reset I_reset(.CLK(fpga_CLK), .NRST(fpga_NRST), .n_rst(n_rst));
-vga #(.vga_HDISP(HDISP), .vga_VDISP(VDISP)) I_vga(.CLK(fpga_CLK_AUX), .NRST(fpga_NRST), .vga_ifm(vga_ifm));
+reset I_reset
+(
+	.CLK(fpga_CLK), 
+	.NRST(fpga_NRST), 
+	.n_rst(n_rst)
+);
+
+vga #(.vga_HDISP(HDISP), .vga_VDISP(VDISP)) I_vga
+(
+	.CLK(fpga_CLK_AUX), 
+	.NRST(fpga_NRST), 
+	.vga_ifm(vga_ifm)
+);
+
+wshb_pll pll
+(
+	.refclk(fpga_CLK),
+	.rst(!fpga_NRST),
+	.outclk_0(wshb_clk),
+	.outclk_1(sdram_clk),
+	.locked(locked)
+);
+
+reset I_reset_wshb
+(
+        .CLK(wshb_clk), 
+        .NRST(fpga_NRST), 
+        .n_rst(wshb_rst)
+);
+
+// Instanciation d'un bush Wishbone 16 bits
+wshb_if #(.DATA_BYTES(2)) wshb_if_0
+(
+	wshb_clk,wshb_rst
+);
+
+// Instanciation du controleur de sdram
+wb16_sdram16 u_sdram_ctrl
+(
+	// Wishbone 16 bits slave interface
+	.wb_s(wshb_if_0.slave),
+	// SDRAM master interface
+	.sdram_m(sdram_ifm),
+	// SDRAM clock
+	.sdram_clk(sdram_clk)
+);
 
 // Assignation des switchs
 assign fpga_LEDR0 = fpga_SW0;
