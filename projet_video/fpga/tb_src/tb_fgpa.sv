e4c676de201ee46853a73aa7c29172b76fe9ff73
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-10-31 23:59:08 +0100

    MaJ: Ajout du sdram dans le testbench

--- projet_video/fpga/tb_src/tb_fgpa.sv
+++ projet_video/fpga/tb_src/tb_fgpa.sv
@@ -9,15 +9,43 @@ wire LEDR0, LEDR1, LEDR2, LEDR3;
 wire SEL_CLK_AUX;
 // interface
 vga_if vga_if0();
+sdram_if sdram_if0();
 
+// Instance du module a tester.
+fpga #(.HDISP(160), .VDISP(90)) I_fpga
+(
+	.fpga_CLK(CLK), 
+	.fpga_CLK_AUX(CLK_AUX),
+	.fpga_SW0(SW0), 
+	.fpga_SW1(SW1), 
+	.fpga_NRST(NRST), 
+	.fpga_LEDR0(LEDR0),
+	.fpga_LEDR1(LEDR1), 
+	.fpga_LEDR2(LEDR2), 
+	.fpga_LEDR3(LEDR3),
+	.fpga_SEL_CLK_AUX(SEL_CLK_AUX), 
+	.vga_ifm(vga_if0),
+	.sdram_ifm(sdram_if0)
+);
 
-// Instance le module a tester.
-fpga #(.HDISP(160), .VDISP(90)) I_fpga(.fpga_CLK(CLK), .fpga_CLK_AUX(CLK_AUX),
-		.fpga_SW0(SW0), .fpga_SW1(SW1), .fpga_NRST(NRST), .fpga_LEDR0(LEDR0),
-		.fpga_LEDR1(LEDR1), .fpga_LEDR2(LEDR2), .fpga_LEDR3(LEDR3),
-		.fpga_SEL_CLK_AUX(SEL_CLK_AUX), .vga_ifm(vga_if0));
+screen #(.mode(0),.X(160),.Y(90)) screen0
+(
+	.vga_ifs(vga_if0)
+);
 
-screen #(.mode(0),.X(160),.Y(90)) screen0(.vga_ifs(vga_if0));
+sdr #(.Debug(0)) SDRAM
+(
+	.Clk    (sdram_if0.clk     ),
+        .Cke    (sdram_if0.cke     ),
+        .Cs_n   (sdram_if0.cs_n    ),
+        .Ras_n  (sdram_if0.ras_n   ),
+        .Cas_n  (sdram_if0.cas_n   ),
+        .We_n   (sdram_if0.we_n    ),
+        .Addr   (sdram_if0.sAddr   ),
+        .Ba     (sdram_if0.ba      ),
+        .Dq     (sdram_if0.sDQ     ),
+        .Dqm    (sdram_if0.dqm     )
+) ;
 
 // Horloges
 always #10ns CLK = ~CLK; // 50MHz
