08914943e02df4011ab4a5a53f2965cc2cd96882
Erwan CHERIAUX <erwan.cheriaux@telecom-paristech.fr>
2016-11-04 19:10:33 +0100

    Ecriture du module wshb_intercon

--- projet_video/fpga/src/wshb_intercon.sv
+++ projet_video/fpga/src/wshb_intercon.sv
@@ -8,7 +8,16 @@ module wshb_intercon (
 wire clk = wshb_if_0.clk;
 wire rst = wshb_if_0.rst;
 
-typedef enum {IDLE, START, GO, END} state_t;
+assign wshb_if_0.dat_ms = wshb_if_mire.dat_ms; //la mire ecrit dans la sdram
+assign wshb_if_vga.dat_sm = wshb_if_0.dat_sm;  //le vga lit la sdram
+
+//lecture en SDRAM (controleur)
+assign wshb_if_0.cyc = 1'b1;
+assign wshb_if_0.sel = 2'b11;
+assign wshb_if_0.cti = 0;
+assign wshb_if_0.bte = 0;
+
+typedef enum {IDLE, priorite_MIRE, priorite_VGA} state_t;
 state_t state, n_state;
 
 //changement d'état cadencé par clk
@@ -22,10 +31,15 @@ always @(*) begin
   case (state)
     IDLE :
       begin
-      n_state = START;
+      n_state = state;
       end
 
-    START :
+    priorite_MIRE :
+      begin
+      n_state = state;
+      end
+
+    priorite_VGA :
       begin
       n_state = state;
       end
@@ -42,10 +56,35 @@ always @(*) begin
   case (state)
     IDLE :
       begin
+      //mire
+      wshb_if_mire.ack <= 1'b0;
+      //vga
+      wshb_if_vga.ack <= 1'b0;
+      //sdram
+      wshb_if_0.adr = 0;
+      wshb_if_0.we  = 1'b0; //1 = ecriture et 0 = lecture
       end
 
-    START :
+    priorite_MIRE :
       begin
+      //mire
+      wshb_if_mire.ack <= 1'b1;
+      //vga
+      wshb_if_vga.ack <= 1'b0;
+      //sdram
+      wshb_if_0.adr = wshb_if_mire.adr;
+      assign wshb_if_0.we  = 1'b1; //ecriture dans la sdram
+      end
+
+    priorite_VGA :
+      begin
+      //mire
+      wshb_if_mire.ack <= 1'b0;
+      //vga
+      wshb_if_vga.ack <= 1'b1;
+      //sdram
+      wshb_if_0.adr = wshb_if_vga.adr;
+      wshb_if_0.we  = 1'b0; //lecture dans la sdram
       end
 
     default :
